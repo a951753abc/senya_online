83cbc92a0a56fc6df333837ba9bf21e72e612f4e
Ying Huei <ying_huei@jesda.com.tw>
2015-08-05 18:24:32 +0800

    Add bootstrap

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,8 +1,14 @@
 <html>
     <head>
         <title>應用程式名稱 - @yield('title')</title>
+        <!-- Bootstrap -->
+        <link href="{{ URL::asset('/css/bootstrap.min.css') }}" rel="stylesheet">
     </head>
     <body>
+        <!-- jQuery (Bootstrap 所有外掛均需要使用) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <!-- 依需要參考已編譯外掛版本（如下），或各自獨立的外掛版本 -->
+        <script src="{{ URL::asset('/js/bootstrap.min.js') }}"></script>
         @section('sidebar')
             這是主要的側邊欄。
         @show
