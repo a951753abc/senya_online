273e8a7710ce7b9604da05af42b4fa94cf6c3c59
JP6 <a951753abc@gmail.com>
2015-09-29 00:11:54 +0800

    CSS 0.2

--- resources/views/lhtrpgView.blade.php
+++ resources/views/lhtrpgView.blade.php
@@ -4,6 +4,12 @@
     <meta charset="utf-8">
     <link href="{{ URL::asset('/css/lhtrpg.css') }}" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="{{ URL::asset('/js/zeroclip/ZeroClipboard.min.js') }}"></script>
+    <script type="text/javascript">
+        $(function() {
+            var client = new ZeroClipboard($(".clipboard"));
+        });
+    </script>
     <title>skill</title>
 </head>
 <body>
@@ -56,6 +62,40 @@
             </p>
         </div>
     </div>
+    <BR>
+    <button class="clipboard" data-clipboard-target="{{$key+1}}">點我複製原始碼</button>
+    <BR>
+    <textarea id="{{$key+1}}">
+    <? 
+        $html = '<div class="skin" style="padding: 0px;  width: 335px; height: 405px;">';
+        $html .= '<div class="skillWrap">';
+        $html .= '<h3 class="skillTitle">';
+        $html .= $value['特技名稱'];
+        $html .= '</h3>';
+        $html .= '<ul class="skillTags"><li class="skillType">'.$value['型態'].'</li>';
+        if (isset($value['標籤'])) {
+            foreach ($value['標籤'] as $tag) {
+                $html .= '<li class="skillTag">'.$tag.'</li>';
+            }
+        }
+        $html .= '</ul><div class="skillTh" style="width:52px">最大SR</div>';
+        $html .= '<div class="skillTd" style="min-width:15px;">'.$value['最大ＳＲ'].'</div>';
+        $html .= '<div class="skillTh">時機</div>';
+        $html .= '<div class="skillTd" style="width:183px;">'.$value['時機'].'</div>';
+        $html .= '<br><div class="skillTh clear">判定</div><div class="skillTd" style="width:268px;">'.$value['判定'].'</div>';
+        $html .= '<br>';
+        $html .= '<div class="skillTh clear">對象</div><div class="skillTd">'.$value['對象'].'</div>';
+        $html .= '<div class="skillTh">射程</div><div class="skillTd">'.$value['射程'].'</div><br><div class="skillTh clear">代價</div>';
+        $html .= '<div class="skillTd">'.$value['代價'].'</div>';
+        $html .= '<div class="skillTh">限制</div><div class="skillTd">'.$value['限制'].'</div><p class="skillFunction"><b>效果：</b>';
+        $html .= $value['效果'].'</p><hr class="skillHr"><p class="skillFunction"><b>解說：</b>';
+        if (isset($value['解說']))
+            $html .= $value['解說'];
+        $html .= '</p></div></div>';
+        echo htmlspecialchars($html);
+    ?>
+    </textarea>
     @endforeach
 </body>
 </html>
+
